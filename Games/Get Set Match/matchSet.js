227d61ea21e3af83b6b81b37efd57339ae07ddeb
MandyArthur <Daviddcg8@kent.ac.uk>
2018-08-15 12:48:45 +0100

    JS edits to flip card

---
+++ Games/Get Set Match/matchSet.js
@@ -0,0 +1,26 @@
+const cards = document.querySelectorAll('.memory-card'); 
+
+let hasFlippedCard = false; 
+let firstCard, secondCard; 
+
+function flipCard(){
+    this.classList.toggle('flip'); 
+
+    if(!hasFlippedCard){
+        //forst click
+        hasFlippedCard = true; 
+        firstCard = this; 
+    } else {
+        //second click
+        hasFlippedCard = false; 
+        secondCard = this; 
+      
+        // do cards match? 
+        console.log(firstCard.dataset.framework); 
+        console.log(secondCard.dataset.framework); 
+
+    }
+    }
+
+
+cards.forEach(card => card.addEventListener('click', flipCard)); 
