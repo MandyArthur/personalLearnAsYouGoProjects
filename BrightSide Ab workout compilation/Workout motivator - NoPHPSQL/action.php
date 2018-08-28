97204d16cd919cd1c6bf40c9212927c15962fe8f
MandyArthur <Daviddcg8@kent.ac.uk>
2018-08-28 15:14:31 +0100

    Basic Workout Motivator

---
+++ BrightSide Ab workout compilation/Workout motivator - NoPHPSQL/action.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Today you have completed: <?php echo $_POST["Plank"]; ?>, <?php echo $_POST["LSPlank"]; ?>
+You are rocking week 1!!
+
+Keep going!
+
+
+</body>
+</html>
---
+++ BrightSide Ab workout compilation/Workout motivator - NoPHPSQL/action_page.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["Sit-ups"]; ?><br>
+Today you have completed: <?php echo $_POST["vehicle"]; ?>, <?php echo $_POST["vehicle"]; ?>
+You are rocking week 1!!
+
+Keep going!
+
+
+</body>
+</html>
